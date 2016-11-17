ea2e878b7276f363e0c997aad8759907eaa63776
edersinsinsin <eder.padilla97@gmail.com>
2016-11-17 00:30:16 -0600

    acomodado de imagenes listo

---
+++ app/src/main/java/jr/eder/developer/example/com/filtersample/FixImagesActivity.java
@@ -0,0 +1,13 @@
+package jr.eder.developer.example.com.filtersample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FixImagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fix_images);
+    }
+}
